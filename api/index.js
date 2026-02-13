f45802b4f3b6aa63df661dc0a5bb11a78f114729
Maida Israr <maidaisrar19@gmail.com>
2026-02-13 09:25:03 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://mern-estate-apz5.vercel.app",
+    origin: "http://localhost:5173/",
     credentials: true,
   })
 );
