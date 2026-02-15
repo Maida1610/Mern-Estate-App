8d8368738b114fc34937fa411e588f18ea2d6709
Maida Israr <maidaisrar19@gmail.com>
2026-02-15 23:09:49 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
   proxy: {
      '/api': {
-      target:'https://localhost:3000',
+      target:'https://real-estate-one-roan.vercel.app',
       secure: false,
     },
   },
