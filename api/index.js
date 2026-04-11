21cb4a76b6e1e129bbdb0a7b67de6d5c5c1bd126
Maida1610 <maidaisrar19@gmail.com>
2026-04-11 19:54:53 +0500

    update index.js

--- api/index.js
+++ api/index.js
@@ -30,7 +30,9 @@ app.use(
     credentials: true,
   })
 );
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
  
 app.listen(3000, () => {
    console.log('Server is running on Port 3000');
